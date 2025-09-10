92b1693142431dba48a24a8f65ac702423b7050c
Aniket <aniketgupta6394@gmail.com>
2025-09-10 14:26:28 +0530

    Add the lottie for the education panel

--- src/app/components/hompage/education/index.tsx
+++ src/app/components/hompage/education/index.tsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { educations } from "@/app/utils/data/education";
+import Image from "next/image";
+import { BsPersonWorkspace } from "react-icons/bs";
+import lottieFile from '../../../assets/lottie/study.json';
+import AnimationLottie from "../../helper/animation-lottie";
+import GlowCard from "../../helper/glow-card";
 
 export default function index() {
   return (

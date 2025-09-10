ff01c72ea0f8d2558767e58e5e0e5ec92977c589
Aniket <aniketgupta6394@gmail.com>
2025-09-10 13:40:18 +0530

    add Skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeaderSection from "./components/hompage/header-section/index";
 import AboutSection from "./components/hompage/about/";
-import Experience from "./components/hompage/experience"
+import Experience from "./components/hompage/experience";
+import Skills from "./components/hompage/skills";
 
 export default async function Home() {
 
@@ -9,6 +10,7 @@ export default async function Home() {
       <HeaderSection />
       <AboutSection />
       <Experience />
+      <Skills />
     </div>
   )
 };

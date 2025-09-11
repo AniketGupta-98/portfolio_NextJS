7b43ddf552a6c864205ba44777c06b82877831cf
Aniket <aniketgupta6394@gmail.com>
2025-09-11 10:45:48 +0530

    profile pic update according to theme

--- src/app/utils/data/experience.tsx
+++ src/app/utils/data/experience.tsx
@@ -2,13 +2,13 @@ export const experiences = [
   {
     id: 1,
     title: 'Software Engineer',
-    company: "Dvertex Private Ltd.",
+    company: "Dvertex Info Private Ltd.",
     duration: "(Oct 2023 - Present)"
   },
   {
     id: 2,
     title: "Software Engineer",
-    company: "Dvertex Private Ltd.",
+    company: "Dvertex Info Private Ltd.",
     duration: "(Apr 2023 - Sept 2023)"
   }
 ]
